3e4d0065c3e80eddd912593090f09f05381d59b1
Khanu <119295270+Khanu123@users.noreply.github.com>
2024-05-25 15:30:08 +0100

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+# utils.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Adding 1 + 2 =", add(1, 2))
+    print("Subtracting 5 - 3 =", subtract(5, 3))
